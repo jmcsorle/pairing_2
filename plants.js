d6fcc235c3c65c2dbd99b65088ecfece78b7768a
Jan McSorley <mcsorleyjan@gmail.com>
2023-03-30 16:02:26 -0500

    Initial commit

---
+++ plants.js
@@ -0,0 +1,9 @@
+var gets_sunshine = true
+var ounces_of_water = 10
+
+if (gets_sunshine === true && ounces_of_water >= 10) {
+    console.log("This plant will thrive.");
+}
+else {
+    console.log("This plant will not thrive.");
+}
