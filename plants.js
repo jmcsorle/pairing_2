0c6baeaa067f8837708edf55e9c1a6e140b680e3
Jan McSorley <mcsorleyjan@gmail.com>
2023-03-30 16:07:56 -0500

    Update plants file.

--- plants.js
+++ plants.js
@@ -1,8 +1,11 @@
-var gets_sunshine = true
+var gets_sunshine = false
 var ounces_of_water = 10
 
 if (gets_sunshine === true && ounces_of_water >= 10) {
     console.log("This plant will thrive.");
+}
+else if (gets_sunshine === true || ounces_of_water >= 10) {
+    console.log("You only received one source of nutrients.");
 }
 else {
     console.log("This plant will not thrive.");
